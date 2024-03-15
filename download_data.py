3ced5b56e27aafaabeff874c3f6ef617f812648f
Daniyar <daniyar.kbv@gmail.com>
2024-03-15 17:53:44 -0400

    refactor: chart texts and reusability

--- download_data.py
+++ download_data.py
@@ -5,15 +5,17 @@ from read_csv import get_usage
 import datetime as dt
 import re
 
-jsession_id = '0000DPQDpc8s513fAxVGuKpivDU:1gupvju42'
+JSESSION_ID = '0000DPQDpc8s513fAxVGuKpivDU:1gupvju42'
+FOLDER_NAME = 'name'
 
 start_date = dt.date(2023, 3, 1)
 end_date = dt.date(2024, 3, 12)
+
 dates = [dt.date.strftime(end_date - dt.timedelta(days=i), '%Y-%m-%d') for i in range(0, (end_date - start_date).days + 1)]
 
 for date in dates:
-    response = request_hourly(jsession_id, date)
-    save_to_csv(response.text)
+    response = request_hourly(JSESSION_ID, date)
+    save_to_csv(response.text, folder_name=FOLDER_NAME)
 
 dfs = []
 for date in dates:
